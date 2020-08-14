62fd4766333e6b9360596e3fbdda7f3973e70278
ayz-lex <andrewyzhanglex@gmail.com>
2020-08-14 12:01:43 -0400

    Updates

---
+++ precache-manifest.ef5209618a4334c37b6121460f58460b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69958e907703795e94779bd9767543ab",
+    "url": "/course-tree/index.html"
+  },
+  {
+    "revision": "939db0a01426f9098335",
+    "url": "/course-tree/static/js/2.33c5e2e6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/course-tree/static/js/2.33c5e2e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06ef6ec81ec3f419093d",
+    "url": "/course-tree/static/js/main.051a986a.chunk.js"
+  },
+  {
+    "revision": "685d35f6452f8dd9fce5",
+    "url": "/course-tree/static/js/runtime-main.5e73d875.js"
+  }
+]);
